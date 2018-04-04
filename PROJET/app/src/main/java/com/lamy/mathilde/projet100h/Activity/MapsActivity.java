02ebeef6f1c8f4464bc32a77b61be69f885cedda
caronarthur <arthur.caron@hei.yncrea.fr>
2018-04-04 12:23:19 +0200

    Maps
    Travaille sur la map

--- PROJET/app/src/main/java/com/lamy/mathilde/projet100h/Activity/MapsActivity.java
+++ PROJET/app/src/main/java/com/lamy/mathilde/projet100h/Activity/MapsActivity.java
@@ -20,53 +20,23 @@ import com.lamy.mathilde.projet100h.R;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    MapView mMapView;
-    View mView;
-    private DatabaseReference mDatabase;
-
-    public MapsActivity() {
-        // Constructeur vide indispensable
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker and move the camera
-        LatLng posEvent = new LatLng(50.629728, 3.043672);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(posEvent, 10));
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mMap.setMyLocationEnabled(true);
+        // Add a marker in Université Catholique de Lille, and move the camera.
+        LatLng sydney = new LatLng(50.633109, 3.046684);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marqueur sur l'Université Catholique de Lille"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-
 }
